331e0b5ecba5933111b2b3283eabe186ea220a0a
Thirhag <meigraf@hotmail.de>
2023-02-06 09:37:11 +0100

    11

---
+++ com/cc/java/HoneyBee.java
@@ -0,0 +1,13 @@
+package com.cc.java;
+
+public class HoneyBee {
+    
+    public String doYourJob() {
+        return "I´m a Honeybee!";
+    }
+    public String fly() {
+        return "i can fly!";
+    }
+
+}
+
