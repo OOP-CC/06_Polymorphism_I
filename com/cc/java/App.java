dd77c66fc7c9002ab94b7f2af46a45df6bab73ac
Peter <schmidt.multimedia@gmail.com>
2023-02-06 10:35:38 +0100

    1. Variante Overloading

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,16 +8,36 @@ public class App {
         Worker worker = new Worker();
         Drone drone = new Drone();
 
-        output(queen.doYourJob());
-        output(worker.doYourJob());
-        output(drone.doYourJob());
+        // output(queen.doYourJob());
+        // output(queen.fly());
 
-        output("--------------------");
+        pollObj(queen);
 
-        output(queen.fly());
-        output(worker.fly());
-        output(drone.fly());
+        // output(worker.doYourJob());
+        // output(worker.fly());
+        pollObj(worker);
 
+        // output(drone.doYourJob());
+        // output(drone.fly());
+        pollObj(drone);
+
+    }
+
+    // 1. Variante Overloading
+
+    private static void pollObj(Queen bee) {
+        output(bee.doYourJob());
+        output(bee.fly());
+    }
+
+    private static void pollObj(Worker bee) {
+        output(bee.doYourJob());
+        output(bee.fly());
+    }
+
+    private static void pollObj(Drone bee) {
+        output(bee.doYourJob());
+        output(bee.fly());
     }
 
 
--- com/cc/java/HoneyBee.java
+++ com/cc/java/HoneyBee.java
@@ -2,9 +2,9 @@ package com.cc.java;
 
 public class HoneyBee {
     
-    public String doYourJob() {
-       return "I'm a Honeybee!" ;
-    }
+    // public String doYourJob() {
+    //    return "I'm a Honeybee!" ;
+    // }
 
     public String fly() {
         return "I can fly!";
