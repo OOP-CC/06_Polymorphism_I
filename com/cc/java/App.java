6bba0de1a42f7d0c35d0f272579bab109f1075ac
Thirhag <meigraf@hotmail.de>
2023-02-06 10:35:55 +0100

    44

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -7,20 +7,35 @@ public class App {
         Queen queen = new Queen();
         Worker worker = new Worker();
         Drone drone = new Drone();
+    
 
-        output(queen.doYourJob());
-        output(worker.doYourJob());
-        output(drone.doYourJob());
+        
+        pollObj(queen);
+        pollObj(drone);
+        pollObj(worker);
+        // output(queen.doYourJob());
+        // output(worker.doYourJob());
+        // output(drone.doYourJob());
 
         output("--------------------");
 
-        output(queen.fly());
-        output(worker.fly());
-        output(drone.fly());
-
-
+        // output(queen.fly());
+        // output(worker.fly());
+        // output(drone.fly());
     }
-
+    
+    private static void pollObj(Queen bee) {
+        output(bee.doYourJob());
+        output(bee.fly());
+    }
+    private static void pollObj(Drone bee) {
+        output(bee.doYourJob());
+        output(bee.fly());
+    }
+    private static void pollObj(Worker bee) {
+        output(bee.doYourJob());
+        output(bee.fly());
+    }
     private static void output(String outputStr) {
         System.out.println(outputStr);
     }
--- com/cc/java/Queen.java
+++ com/cc/java/Queen.java
@@ -1,5 +1,7 @@
 package com.cc.java;
 
 public class Queen extends HoneyBee{
-    
+    public String doYourJob() {
+        return "I'm a Queen, any questions?";
+     } 
 }
